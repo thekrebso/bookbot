81fb2dbc91056c5bf9f85ba9b18f76c942bc7e89
krebso <krebso@krebso.pl>
2025-05-16 02:07:23 +0200

    add letter counting

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words
+from stats import get_num_words, get_num_letters
 
 
 def get_book_text(filepath):
@@ -10,7 +10,10 @@ def get_book_text(filepath):
 def main():
     text = get_book_text("./books/frankenstein.txt")
     words_num = get_num_words(text)
+    letters_num = get_num_letters(text)
+
     print(f"{words_num} words found in the document")
+    print(letters_num)
 
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,17 @@
 
 def get_num_words(text):
     return len(text.split())
+
+
+def get_num_letters(text):
+    letters = {}
+
+    for letter in text:
+        letter = letter.lower()
+
+        if letter in letters:
+              letters[letter] += 1
+        else:
+            letters[letter] = 1
+
+    return letters
