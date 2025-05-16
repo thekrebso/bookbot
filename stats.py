ee8c39c8e8647d744d5eae36a079d8586be90627
krebso <krebso@krebso.pl>
2025-05-16 02:24:59 +0200

    add letter sort by num

--- stats.py
+++ stats.py
@@ -15,3 +15,15 @@ def get_num_letters(text):
             letters[letter] = 1
 
     return letters
+
+
+def get_sorted_letters(letters):
+    result = []
+
+    for letter in letters:
+        if letter.isalpha():
+            result.append({ "char": letter, "num": letters[letter] })
+
+    result.sort(reverse=True, key=lambda x: x["num"])
+
+    return result
